5854c8a76289891a39f4a459b8d3c6059a1aba73
alexander.fournier <alexander.fournier@nasm.org>
2018-03-07 16:52:34 -0700

    database

--- com-spring-rest/src/main/java/com/example/demo/controller/HomeController.java
+++ com-spring-rest/src/main/java/com/example/demo/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-	
 	@RequestMapping("/")
 	public String home(){
 		return "NASM APP LAUNCH" ;
